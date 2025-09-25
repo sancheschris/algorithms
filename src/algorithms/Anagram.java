d4b1dbe529dc7ed6cd232419c9dea2a51348551f
Christian Sanches <christian.sanches-v@uplevelteam.com>
2025-09-24 22:48:55 -0300

    anagram with sort and a single maap

--- src/algorithms/Anagram.java
+++ src/algorithms/Anagram.java
@@ -1,5 +1,6 @@
 package algorithms;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,6 +9,10 @@ public class Anagram {
         System.out.println(isAnagram("anagram", "nagaram"));
         System.out.println(isAnagram("anagram", "nagarame"));
         System.out.println(isAnagram2("anagram", "nagaram"));
+        System.out.println(isAnagramSort("silent", "nagaram"));
+
+
+        System.out.println(isAnagramMap("rat", "tar"));
     }
 
 
@@ -23,11 +28,17 @@ public class Anagram {
             count[t.charAt(i) - 'a']--;
         }
 
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) {
+        for (int value : count) {
+            if (value != 0) {
                 return false;
             }
         }
+
+//        for (int i = 0; i < 26; i++) {
+//            if (count[i] != 0) {
+//                return false;
+//            }
+//        }
         return true;
     }
 
@@ -46,6 +57,42 @@ public class Anagram {
 
         return countS.equals(countT);
     }
+
+    public static boolean isAnagramMap(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> count = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            count.put(s.charAt(i), count.getOrDefault(s.charAt(i), 0) + 1);
+            count.put(t.charAt(i), count.getOrDefault(t.charAt(i), 0) - 1);
+        }
+
+        for (int value : count.values()) {
+            if (value != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isAnagramSort(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+
+        return Arrays.equals(sArray, tArray);
+    }
+
+
 }
 
 
