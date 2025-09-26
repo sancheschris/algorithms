1d7d19520e16c318c57d63b0be5fc7c8c93e7335
Christian Sanches <christian.sanches-v@uplevelteam.com>
2025-09-25 22:33:31 -0300

    implementing twoSum bruteforce

--- src/algorithms/TwoSum.java
+++ src/algorithms/TwoSum.java
@@ -1,8 +1,22 @@
 package algorithms;
 
+import java.util.Arrays;
+
 public class TwoSum {
 
     public static void main(String[] args) {
+        int[] arr = twoSumBruteForce(new int[]{2, 7, 11, 15}, 9);
+        System.out.println(Arrays.toString(arr));
+    }
 
+    public static int[] twoSumBruteForce(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
     }
 }
