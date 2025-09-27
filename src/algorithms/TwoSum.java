b3b33c83fca533020fb581de4fa016a38e287bd3
Christian Sanches <christian.sanches-v@uplevelteam.com>
2025-09-26 22:45:04 -0300

    enhanced two sum with two pointers

--- src/algorithms/TwoSum.java
+++ src/algorithms/TwoSum.java
@@ -7,6 +7,9 @@ public class TwoSum {
     public static void main(String[] args) {
         int[] arr = twoSumBruteForce(new int[]{2, 7, 11, 15}, 9);
         System.out.println(Arrays.toString(arr));
+
+        int[] arr2 = twoSum(new int[]{2, 7, 11, 15}, 26);
+        System.out.println(Arrays.toString(arr2));
     }
 
     public static int[] twoSumBruteForce(int[] nums, int target) {
@@ -15,6 +18,23 @@ public class TwoSum {
                 if (nums[i] + nums[j] == target) {
                     return new int[]{i, j};
                 }
+            }
+        }
+        return new int[]{};
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == target) {
+                return new int[]{left, right};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
             }
         }
         return new int[]{};
