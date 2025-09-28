eb737085252600a38dc47c2c35ed6e5a9a8996dc
Christian Sanches <christian.sanches-v@uplevelteam.com>
2025-09-28 18:13:26 -0300

    rotateArray improved

--- src/algorithms/RotateArray.java
+++ src/algorithms/RotateArray.java
@@ -5,8 +5,12 @@ import java.util.Arrays;
 public class RotateArray {
 
     public static void main(String[] args) {
-        int[] nums = rotateArray(new int[]{1, 2, 3, 4, 5, 6, 7}, 3);
+//        int[] nums = rotateArray(new int[]{1, 2, 3, 4, 5, 6, 7}, 3);
 
+//        System.out.println(Arrays.toString(nums));
+
+        int[] nums = rotateArray(new int[]{1, 2, 3, 4, 5, 6, 7}, 2);
+        rotateArrayImproved(nums, 2);
         System.out.println(Arrays.toString(nums));
     }
 
@@ -22,6 +26,25 @@ public class RotateArray {
             arr[0] = last;
         }
         return arr;
+    }
+
+    public static void rotateArrayImproved(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n;
+
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+    }
+
+    public static void reverse(int[] arr, int left, int right) {
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
     }
 }
 
