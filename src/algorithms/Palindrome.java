a590ee5c4f834aefd8a1a625cf6b84f48b0467a9
Christian Sanches <christian.sanches-v@uplevelteam.com>
2025-09-28 18:55:46 -0300

    implemented the enhanced version of isPalindrome

--- src/algorithms/Palindrome.java
+++ src/algorithms/Palindrome.java
@@ -4,6 +4,9 @@ public class Palindrome {
     public static void main(String[] args) {
         System.out.println(isPalindromeBruteForce("Was it a car or a cat I saw?"));
         System.out.println(isPalindromeBruteForce("tab a cat"));
+
+
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
     }
 
     public static boolean isPalindromeBruteForce(String s) {
@@ -38,6 +41,29 @@ public class Palindrome {
                return false;
            }
        }
+        return true;
+    }
+
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
         return true;
     }
 
