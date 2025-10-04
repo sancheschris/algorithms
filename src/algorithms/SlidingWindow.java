7a8ddce92b554bdc585632eb869cfb88519ca425
Christian Sanches <christian.sanches-v@uplevelteam.com>
2025-10-04 13:14:11 -0300

    maxProfit using brutal force

--- src/algorithms/SlidingWindow.java
+++ src/algorithms/SlidingWindow.java
@@ -3,6 +3,8 @@ package algorithms;
 public class SlidingWindow {
     public static void main(String[] args) {
         System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+
+        System.out.println(maxProfitBrutalForce(new int[]{7, 6, 4, 3, 1}));
     }
 
     public static int maxProfit(int[] prices) {
@@ -18,6 +20,20 @@ public class SlidingWindow {
                 left = right;
             }
             right++;
+        }
+        return maxProfit;
+    }
+
+    public static int maxProfitBrutalForce(int[] prices) {
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[i] < prices[j]) {
+                    int profit = prices[j] - prices[i];
+                    maxProfit = Math.max(maxProfit, profit);
+                }
+            }
         }
         return maxProfit;
     }
