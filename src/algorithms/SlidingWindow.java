357965404552a570408dd1daae009d35d88a8edd
Christian Sanches <christian.sanches-v@uplevelteam.com>
2025-10-03 23:04:09 -0300

    implementing sliding window

---
+++ src/algorithms/SlidingWindow.java
@@ -0,0 +1,24 @@
+package algorithms;
+
+public class SlidingWindow {
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int left = 0;
+        int right = 1;
+        int maxProfit = 0;
+
+        while (right < prices.length) {
+            if (prices[left] < prices[right]) {
+                int profit = prices[right] - prices[left];
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                left = right;
+            }
+            right++;
+        }
+        return maxProfit;
+    }
+}
