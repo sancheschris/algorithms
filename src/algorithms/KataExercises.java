e41a4182e46d38e42ea2fb23a88d761087379fd4
Christian Sanches <christian.sanches-v@uplevelteam.com>
2025-10-05 16:49:55 -0300

    summing up all the multiples of 3 or 5 below 10

--- src/algorithms/KataExercises.java
+++ src/algorithms/KataExercises.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 public class KataExercises {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(countBy(5, 5)));
+
+        System.out.println(solution(10));
     }
 
     //Create a function with two arguments that will return an array of the first n multiples of x.
@@ -44,6 +46,21 @@ public class KataExercises {
             }
         }
         return "";
+    }
+
+    //    if we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+    //
+    //Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+    //
+    //Note: If a number is a multiple of both 3 and 5, only count it once.
+    public static int solution(int number) {
+        int sum = 0;
+        for (int i = 0; i < number; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
     }
 }
 
