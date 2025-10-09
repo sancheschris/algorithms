7c3e345c71453cf382dfde54b0bc7844c3d1c6e3
Christian Sanches <christian.sanches-v@uplevelteam.com>
2025-10-08 22:04:11 -0300

    brute force

--- src/algorithms/LinkedList.java
+++ src/algorithms/LinkedList.java
@@ -1,12 +1,22 @@
 package algorithms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LinkedList {
 
 
     public static void main(String[] args) {
         ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, null)));
         ListNode reversedHead = reverseList(head);
-        System.out.println(reversedHead.val); // 3
+        System.out.println(reversedHead); // 3
+
+        ListNode head2 = new ListNode(1, new ListNode(2, new ListNode(3, null)));
+        ListNode reversedHead2 = reverseListBruteForce(head2);
+
+        for (ListNode curr = reversedHead2; curr != null; curr = curr.next) {
+            System.out.print(curr.val + " ");
+        }
     }
 
     public static ListNode reverseList(ListNode head) {
@@ -22,11 +32,36 @@ public class LinkedList {
         return prev;
     }
 
+    public static ListNode reverseListBruteForce(ListNode head) {
+
+        List<Integer> values = new ArrayList<>();
+        ListNode curr = head;
+
+        while (curr != null) {
+            values.add(curr.val);
+            curr = curr.next;
+        }
+
+        ListNode newHead = null;
+        for (int i = values.size() - 1; i >= 0; i--) {
+            newHead = new ListNode(values.get(i), newHead);
+        }
+        return newHead;
+    }
+
     static class ListNode {
         int val;
         ListNode next;
         ListNode() {}
         ListNode(int val) { this.val = val; }
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    '}';
+        }
+    }
 }
